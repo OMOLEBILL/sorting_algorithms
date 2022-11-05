5ae319ba9cdffd9b1b99c4e1fb90e9d4fd8be4fe
OMOLEBILL <omolebill01@gmail.com>
2022-11-05 09:38:42 +0000

    added a bubblesort fumction that works on a array of intergers

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * bubble_sort - a function that sort an @array of intergers of size @size
+ * in ascending order using the bubble sort algorithm
+ * @array: array of interegers to be sorted of size @size
+ * @size: the size of @array to be sorted
+ * Return: nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, swap, temp;
+
+	swap = 0;
+	temp = 0;
+	for(i = 0; i < size; i++)
+	{
+		for(j = i; j < size - 1; j++)
+		{
+			if(array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swap++;
+				print_array(array, size);
+			}
+		}
+		if(!swap)
+			break;
+	}
+}
