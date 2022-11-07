2c419f7e207be1609cbf27c9d5c862fed52e3f08
OMOLEBILL <omolebill01@gmail.com>
2022-11-07 15:51:33 +0000

    Insertion sort for a double linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list in ascending order using
+ * insertion algorithm
+ *
+ * @list: address of head node of double linked list being sorted
+ * Return: nothing sorts in place
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = NULL, *previous = NULL, *next = NULL;
+
+	current = *list;
+	while (current)
+	{
+		next  = current->next;
+		previous = current->prev;
+		while (previous)
+		{
+			if (current->n < previous->n)
+			{
+				swap(list, current, previous);
+				print_list(*list);
+			}
+			previous = previous->prev;
+		}
+		current = next;
+	}
+}
+
+/**
+ * swap - exchanges two out of ascending order nodes of a doubly linked list
+ * to their respective in order places
+ *
+ * @current: out of order integer supposed to be in @previous's place
+ * @previous: out of order integer supposed to be in @current's place
+ *
+ * Return: exchanges out of place with no extra memory
+ */
+void swap(listint_t **list, listint_t *current, listint_t *previous)
+{
+	if (previous->prev == NULL)
+	{
+		previous->next = current->next;
+		current->next->prev = previous;
+		current->prev = previous->prev;
+		previous->prev = current;
+		current->next = previous;
+		*list = current;
+	}
+	else if (current->next == NULL)
+	{
+		previous->next = current->next;
+		current->prev = previous->prev;
+		previous->prev->next = current;
+		previous->prev = current;
+		current->next = previous;
+	}
+	else
+	{
+		previous->next = current->next;
+		current->next->prev = previous;
+		current->prev = previous->prev;
+		previous->prev->next = current;
+		previous->prev = current;
+		current->next = previous;
+	}
+}
